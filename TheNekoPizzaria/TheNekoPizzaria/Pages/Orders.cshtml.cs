8c0d3574460fd215e0f93fd283070a94788c267b
79384 <smb_udk@vk.com>
2023-12-16 11:04:08 +0700

    added Pizza page where all the orders are displayed"

---
+++ TheNekoPizzaria/TheNekoPizzaria/Pages/Orders.cshtml.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using TheNekoPizzaria.Data;
+using TheNekoPizzaria.Models;
+
+namespace TheNekoPizzaria.Pages
+{
+    public class OrdersModel : PageModel
+    {
+        public List<PizzaOrder> PizzaOrders = new List<PizzaOrder>();
+
+        private readonly ApplicationDbContext _context;
+
+        public OrdersModel(ApplicationDbContext context) { 
+        _context = context;
+        }
+
+        public void OnGet()
+        {
+            PizzaOrders = _context.PizzaOrders.ToList();
+        }
+    }
+}
--- TheNekoPizzaria/TheNekoPizzaria/obj/Debug/net6.0/TheNekoPizzaria.AssemblyInfo.cs
+++ TheNekoPizzaria/TheNekoPizzaria/obj/Debug/net6.0/TheNekoPizzaria.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TheNekoPizzaria")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8a01db50dac7e1388f20f4d4522171b43d7a69c6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc3c5f1a0ccd0206c3598d9b0619bf819582d093")]
 [assembly: System.Reflection.AssemblyProductAttribute("TheNekoPizzaria")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TheNekoPizzaria")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
